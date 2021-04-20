ae4c234c7ff4cc0deb687c054b2bb7b232ce8ddd
wind1996 <2924456233@qq.com>
2021-04-20 18:36:52 +0800

    [M]添加一些组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Layout, Menu, Divider,
+} from 'antd';
+import { HashRouter as Router, Link, useHistory } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes';
+import { name } from '../package.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const {
+  Content, Footer, Sider,
+} = Layout;
+
+const LibLayout = (props) => {
+  const { location } = useHistory();
+  console.error(1111111111, props, location);
+
+  return <Layout>
+    <Sider
+      theme="light"
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+      }}
+    >
+      <div
+        className="logo"
+        style={{
+          background: 'rgba(255, 255, 255, 0.2)',
+          padding: 16,
+          margin: '0 auto',
+          textAlign: 'center',
+          lineHeight: '32px',
+        }}
+      >
+        <Link to={{
+          pathname: '/',
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {name}
+        </Link>
+      </div>
+      <Divider style={{ margin: '0' }} />
+      <Menu mode="inline" defaultSelectedKeys={[location.pathname]}>
+        {routes.map((item) => {
+          if (item.hidden) {
+            return null;
+          }
+          return (
+            <Menu.Item key={item.path}>
+              <Link to={{
+                pathname: item.path,
+              }}
+              >
+                <span className="nav-text">{item.title}</span>
+              </Link>
+            </Menu.Item>
+          );
+        })}
+      </Menu>
+    </Sider>
+    <Layout style={{ marginLeft: 200 }}>
+      {/* <Header style={{background: '#fff', padding: 0}}/> */}
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div style={{ padding: 24, background: '#fff' }}>
+          {renderRoutes(routes)}
+        </div>
+      </Content>
+    </Layout>
+  </Layout>;
+};
 
-export default App;
+export default () => (
+  <Router>
+    <LibLayout />
+  </Router>
+);
